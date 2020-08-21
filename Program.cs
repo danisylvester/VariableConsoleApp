9ea46769104136f206bb5b8435ca2565c6f611d5
danisylvester <maria.sylvester10@gmail.com>
2020-08-21 12:13:22 -0500

    declared, initialized, and printed variables.

--- Program.cs
+++ Program.cs
@@ -6,11 +6,24 @@ namespace VariableConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, what is your name?");
-            string name = Console.ReadLine();
-            Console.WriteLine($"Hello {name} /n" + "How old are you?");
-            int age = Convert.ToInt32(Console.ReadLine());
+            // Variable Declaration
+            int age;
+            string name;
+            bool hasPet;
+            char letter;
+            double gpa;
+            decimal mathGrade;
 
+            // Variable initalizations
+            age = 26;
+            name = "Dani";
+            hasPet = true;
+            letter = 'A';
+            gpa = 3.95;
+            mathGrade = 98.99m;
+
+            Console.WriteLine($"{name} is {age} years old. \n" + $"Does {name} have a pet? {hasPet}.");
+            Console.WriteLine($"{name} has a gpa of {gpa} \n" + $"She has the grad {letter} in math. Her math grade is {mathGrade}.");
         }
     }
 }
