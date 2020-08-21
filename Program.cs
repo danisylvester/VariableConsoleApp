c36cc9b0ba89304af321fd06cda21204cb6f60c6
danisylvester <maria.sylvester10@gmail.com>
2020-08-21 11:21:50 -0500

    Asks name and age

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VariableConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, what is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name} /n" + "How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
